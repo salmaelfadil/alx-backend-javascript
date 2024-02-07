56cb6df26988df17c9166c4619b0e46d9792a735
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-02-07 21:45:33 +0000

    .

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,11 +3,11 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({
         status: 200,
-        body: Success,
+        body: 'Success',
       });
     } else {
       reject(new Error(
-        The fake API is not working currently,
+        'The fake API is not working currently',
       ));
     }
   });
