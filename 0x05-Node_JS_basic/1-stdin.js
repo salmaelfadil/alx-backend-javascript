4ec2e0655ca6094dad7c669e49ffa88605ec5cf0
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-04-05 22:14:26 +0000

    .

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.once('data', (input) => {
-	const name = input.toString().trim();
-	console.log(`Your name is: ${name}`);
+	const name = input.toString();
+	process.stdout.write(`Your name is: ${name}`);
 	process.exit();
 });
 
 process.on('exit', () => {
-	console.log("This important software is now closing");
+	process.stdout.write("This important software is now closing");
 });
